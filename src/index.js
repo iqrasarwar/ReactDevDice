9dea0158cb6f0018f1a3b0aadfe161588ad1bd89
iqrasarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-06-08 01:52:12 +0500

    dy 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+function StateComponent(prop) {
+  const [name, setName] = React.useState({ name: prop.name });
+  function update() {
+    setName(prevStte =>  {return  { ...prevStte, name: "new Name" };
+});
+  }
+  return (
+    <div>
+      <p>Your name here! {name.name}</p>
+      <button OnClick={() => update()}>Click To change Name</button>
+    </div>
+  );
+}
+ReactDOM.render(
+  <StateComponent name="prevname" />,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
