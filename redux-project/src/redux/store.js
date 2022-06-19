89fc3c4a6847c4a1c5d420708ff8348ee4ec3987
iqrasarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-06-20 00:57:52 +0500

    redux boilerplate

---
+++ redux-project/src/redux/store.js
@@ -0,0 +1,9 @@
+import { createStore } from "redux";
+import {reducers} from "./reducers/index";
+
+const store = createStore(
+  reducers,
+  {},
+  window._REDUX_DEVTOOLS_EXTENSION_ && window._REDUX_DEVTOOLS_EXTENSION_()
+);
+export default store;
