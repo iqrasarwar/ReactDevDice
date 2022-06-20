4af68c861894a2c21dbb8098f4b3473de8b5b4be
iqrasarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-06-20 14:06:01 +0500

    products from fake store api

--- redux-project/src/App.js
+++ redux-project/src/App.js
@@ -1,7 +1,18 @@
+import Header from "./containers/Header";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ProductLisitng from "./containers/ProductListing"
+import ProductDetails from "./containers/ProductDetails"
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/"  element={<ProductLisitng />} />
+          <Route path="/product/:productId"  element={<ProductDetails/>} />
+          <Route> Error 404 ! Not Found</Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ redux-project/src/containers/ProductComponent.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+const ProductComponent = () => {
+  const Products = useSelector((state) => state.allProducts.products);
+  const rednderList = Products.map((product) => {
+    const { id, title, image, price, category } = product;
+    return (
+      <div className="four wide column" key={id}>
+        <div className="ui link cards">
+          <div className="card">
+            <div className="image">
+              <img src={image} alt={title} />
+            </div>
+            <div className="content">
+              <div className="header">{title}</div>
+              <div className="meta price">${price}</div>
+              <div className="meta">{category}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  })
+  return <>{rednderList}</>;
+};
+
+export default ProductComponent;
