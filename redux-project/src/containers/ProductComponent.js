0754c9d2651d334320e885da9717758ab22dbc60
iqrasarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-06-24 23:51:28 +0500

    tic tac toe bolier late added

--- redux-project/src/containers/ProductComponent.js
+++ redux-project/src/containers/ProductComponent.js
@@ -1,24 +1,26 @@
 import React from "react";
 import { useSelector } from "react-redux";
-
+import {Link} from "react-router-dom"
 const ProductComponent = () => {
   const Products = useSelector((state) => state.allProducts.products);
   const rednderList = Products.map((product) => {
     const { id, title, image, price, category } = product;
     return (
       <div className="four wide column" key={id}>
-        <div className="ui link cards">
-          <div className="card">
-            <div className="image">
-              <img src={image} alt={title} />
-            </div>
-            <div className="content">
-              <div className="header">{title}</div>
-              <div className="meta price">${price}</div>
-              <div className="meta">{category}</div>
+        <Link to={`./product/${id}`}>
+          <div className="ui link cards">
+            <div className="card">
+              <div className="image">
+                <img src={image} alt={title} />
+              </div>
+              <div className="content">
+                <div className="header">{title}</div>
+                <div className="meta price">${price}</div>
+                <div className="meta">{category}</div>
+              </div>
             </div>
           </div>
-        </div>
+        </Link>
       </div>
     );
   })
